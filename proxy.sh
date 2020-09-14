6150d96c9c012850b6eede076e0d25b0f86b2a79
wangchunin <1104436919@qq.com>
2020-09-14 23:42:24 +0800

    jug

--- proxy.sh
+++ proxy.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # start caddy
-
-caddy reverse-proxy  -to $PROXY_HOST --from :$PORT  --change-host-header
-
+if [ $PASS_ORIGIN = false ];then
+   caddy reverse-proxy  -to $PROXY_HOST --from :$PORT  --change-host-header
+elif [ $PASS_ORIGIN = true ];then
+   caddy reverse-proxy  -to $PROXY_HOST --from :$PORT
+else
+   echo "Wrong PASS_ORIGIN value!"
+fi
 
