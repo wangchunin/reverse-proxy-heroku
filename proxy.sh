95988ab54e4dd4ef3731d72358e3f650526ed175
wangchunin <1104436919@qq.com>
2020-09-14 22:13:59 +0800

    2

--- proxy.sh
+++ proxy.sh
@@ -3,6 +3,6 @@
 # start caddy
 nohup tor &
 
-caddy reverse-proxy  -to ishare.melulu.workers.dev --from :$PORT  --change-host-header &
+caddy reverse-proxy  -to ishare.melulu.workers.dev --from :$PORT  --change-host-header
 
 
